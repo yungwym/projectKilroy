5b62882dc96527295428d1cf28a23ae11222cc27
Rob Wymer <wymer1120@gmail.com>
2017-07-22 15:26:25 -0400

    Initial Commit

---
+++ CraftON./ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CraftON.
+//
+//  Created by Alex Wymer  on 2017-07-22.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CraftON./main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CraftON.
+//
+//  Created by Alex Wymer  on 2017-07-22.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
